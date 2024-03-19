43c21d43f52e3f1843b3623bc3d319a1ef462f28
YoussefAhmed99 <youssefahmedb90@gmail.com>
2024-03-19 07:27:44 +0200

    Added Adtactix/Autopilot

--- src/server.js
+++ src/server.js
@@ -3,20 +3,12 @@ const app = express();
 const authenticate = require('../middleware/authentication');
 const toolRouters = require('../routes/tools/tools');
 
-// Use the authentication middleware globally
-// If you want to protect only certain routes, you can apply it specifically instead
-app.use(express.json());
+
+app.use(express.json({ limit: '400mb' }));
 app.use(authenticate);
 
-// Example protected route
-app.use('/api/protected', authenticate, (req, res) => {
-    res.send('Access to protected resource granted');
-});
-
-// Set up tool routes
 app.use('/api/rankspy', toolRouters.rankSpy);
-
-// ... rest of the server configuration
+app.use('/api/adtactix', toolRouters.adtactix);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
